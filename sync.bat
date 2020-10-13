21cb81b1052a3a4ecace3422d3b95e339287e02f
Liam <liamaiscool8@gmail.com>
2020-10-13 18:24:15 -0400

    Updated sync.bat

--- sync.bat
+++ sync.bat
@@ -1,3 +1,3 @@
 @echo off
 echo Syncing files
-xcopy C:\Users\Liam\Documents\GitHub\Batch-Scripts C:\Windows\System32
+xcopy C:\Users\Liam\OneDrive\Desktop\VSC Projects\Batch Scripts C:\Windows\System32
